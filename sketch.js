fadf32eb1c680bb1b539c34a2f348cb6c5b7a2b5
Jotbir singh <77562724+jotbirsingh@users.noreply.github.com>
2021-05-22 18:45:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+
+function preload(){
+  backgroundImg =loadImage("snow2.jpg");
+}
+
+function setup() { 
+  createCanvas(1200,600);
+ 
+}
+
+function draw() {
+  background(backgroundImg);  
+  drawSprites();
+ 
+    
+}
